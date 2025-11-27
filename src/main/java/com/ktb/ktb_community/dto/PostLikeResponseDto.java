abb61e126d4e0260f853ac0b86dd06e635595b11
Hyeseong-Myeong <a01025819869@gmail.com>
2025-11-28 08:37:17 +0900

    setting: CI workflows(#11)

--- src/main/java/com/ktb/ktb_community/dto/PostLikeResponseDto.java
+++ src/main/java/com/ktb/ktb_community/dto/PostLikeResponseDto.java
@@ -1,13 +1,12 @@
 package com.ktb.ktb_community.dto;
 
-import com.ktb.ktb_community.entity.PostLike;
 import lombok.Data;
 
 @Data
 public class PostLikeResponseDto {
 
-    Integer likeCount;
-    Boolean isLiked;
+    private Integer likeCount;
+    private Boolean isLiked;
 
     public PostLikeResponseDto(Integer likeCount, Boolean isLiked) {
         this.likeCount = likeCount;
